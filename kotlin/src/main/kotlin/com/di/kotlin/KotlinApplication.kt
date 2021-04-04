54a080998a904ebdc4eeb2ad2abcdd61e1fa391c
Fabian Frangella <fabianfrangella@gmail.com>
2021-04-04 19:25:30 -0300

    base projects

---
+++ kotlin/src/main/kotlin/com/di/kotlin/KotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.di.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinApplication>(*args)
+}
